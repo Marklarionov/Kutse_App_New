be55ad2ab3fbc2a19a46ed621baaeb60267ebfea
opilane <opilane@HP-CZC2349HV0>
2023-01-24 15:57:45 +0200

    Guests

--- Kutse_App/Controllers/HomeController.cs
+++ Kutse_App/Controllers/HomeController.cs
@@ -5,22 +5,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace Kutse_App.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            ViewBag.Message = "Ootan sind minu peole! Palun tule!";
+        {              
             int month = DateTime.Now.Month;
-            ViewBag.Greeting = month == 1 ? "Rahupäev" : ViewBag.Greeting = month == 2 ? "Pehme padja päev" : ViewBag.Greeting = month == 3 ? "Šokolaadi kirsipäev" : 
-                ViewBag.Greeting = month == 4 ? "spagettide päev" : ViewBag.Greeting = month == 5 ? "" : ViewBag.Greeting = month == 6 ? "" : ViewBag.Greeting = month == 7 ? ""
-                : ViewBag.Greeting = month == 8 ? "" : ViewBag.Greeting = month == 9 ? "" : ViewBag.Greeting = month == 10 ? "" : ViewBag.Greeting = month == 11 ? ""
-                : "";
+            ViewBag.Greeting2 = month == 1 ? "rahupäev" : ViewBag.Greeting2 = month == 2 ? "pehme padja päev" : ViewBag.Greeting2 = month == 3 ? "šokolaadi kirsipäev" : 
+                ViewBag.Greeting2 = month == 4 ? "spagettide päev" : ViewBag.Greeting2 = month == 5 ? "päev 5" : ViewBag.Greeting2 = month == 6 ? "päev 6" : ViewBag.Greeting2 = month == 7 ? "päev 7"
+                : ViewBag.Greeting2 = month == 8 ? "päev 8" : ViewBag.Greeting2 = month == 9 ? "päev 9" : ViewBag.Greeting2 = month == 10 ? "päev 10" : ViewBag.Greeting2 = month == 11 ? "päev 11"
+                : "päev 12";
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour >= 0 && hour <= 6 ? "Head õõd" : ViewBag.Greeting = hour >= 6 && hour <= 12 ? "Tere hommikust" :
-                ViewBag.Greeting = hour >= 12 && hour <= 18 ? "Head päevast" : "Tere õhtust";
+                ViewBag.Greeting = hour >= 12 && hour <= 18 ? "Tere päevast" : "Tere õhtust";
             return View();
         }
         [HttpGet]
@@ -33,7 +33,11 @@ namespace Kutse_App.Controllers
         {
             E_mail(guest);
             if (ModelState.IsValid)
-            { return View("Thanks", guest); }
+            { 
+                db.Guests.Add(guest);
+                db.SaveChanges();
+                return View("Thanks", guest); 
+            }
             else
             { return View(); }
         }
@@ -56,7 +60,44 @@ namespace Kutse_App.Controllers
                 ViewBag.Message = "Mul on kahju!Ei saa kirja saada!";
             }
         }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Guest guest)
+        {
+            db.Guests.Add(guest);
+            db.SaveChanges();
+            return RedirectToAction("Guests");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id) 
+        {
+            Guest g = db.Guests.Find(id);
+            if (g==null)
+            {
+                return HttpNotFound();
+            }
+            return View(g);
+        }
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id) 
+        {
+            Guest g = db.Guests.Find(id);
+            if (g == null)
+            {
+                return HttpNotFound();
+            }
+            db.Guests.Remove(g);
+            db.SaveChanges();
+            return RedirectToAction("Guests");
+        }
+
+
         GuestContext db = new GuestContext();
+        [Authorize]
         public ActionResult Guests()
         {
             IEnumerable<Guest> guests = db.Guests;
